ae5116d4b802f31ff24a58960a2d768c412d2cc4
Tim Buschtöns <tbuschto@eclipsesource.com>
2014-07-18 13:26:24 +0200

    Fix/add Copyright header and update README.md

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 rap-chartjs
 ===========
 
-chartjs as RAP CustomWidget
+* Chart.js as a RAP CustomWidget
+* Experimental state, but should work fine except on IE7/8
+* Contributions are welcome
+* Published under the Eclipse Public License - v 1.0 (see LICENSE)
+* Chart.js is available under the [MIT license](http://opensource.org/licenses/MIT) (see docs/chartjs-LICENSE.md)
--- bundles/org.eclipse.rap.chartjs/src/org/eclipse/rap/chartjs/internal/ResourceLoaderUtil.java
+++ bundles/org.eclipse.rap.chartjs/src/org/eclipse/rap/chartjs/internal/ResourceLoaderUtil.java
@@ -1,6 +1,5 @@
-package org.eclipse.rap.chartjs.internal;
 /*******************************************************************************
- * Copyright (c) 2012, 2013 EclipseSource and others.
+ * Copyright (c) 2013, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +9,7 @@ package org.eclipse.rap.chartjs.internal;
  *    EclipseSource - initial API and implementation
  ******************************************************************************/
 
+package org.eclipse.rap.chartjs.internal;
 
 import java.io.BufferedReader;
 import java.io.IOException;
